689408d1a98be14f59696487992c763cf475397a
Peter Sharp <peter@webmad.co.nz>
2018-12-10 10:27:15 +1300

    got working in FF again, fixed incorrect time duration

--- main.js
+++ main.js
@@ -3,14 +3,19 @@ import moment from 'https://unpkg.com/moment@2.22.2/src/moment.js'
 console.log('timeleft started')
 
 const $ = document.querySelector.bind(document)
-const hoursPerWeek = '40:00'
+const hoursPerWeek = 40
 let $summaryRow = $('.max-summary').querySelector('tbody tr');
 let $totalCol = $summaryRow.querySelector('td:last-child');
 let $leftCol = $totalCol.cloneNode(true);
-let complete = $totalCol.querySelector('.max-summary-text').innerHTML
-let left = moment.duration(hoursPerWeek).subtract(moment.duration(complete))
+let complete = toAspMsDuration($totalCol.querySelector('.max-summary-text').innerHTML)
 
-$leftCol.querySelector('.max-summary-text').innerHTML = `${left.hours()}:${left.minutes()}`
+let left =  moment.duration(hoursPerWeek, 'hours').subtract(moment.duration(complete))
+
+$leftCol.querySelector('.max-summary-text').innerHTML =  `${Math.floor(left.asHours())}:${left.minutes()}`
 $leftCol.querySelector('.max-summary-heading').innerHTML = 'Left'
 $summaryRow.appendChild($leftCol);
+
+function toAspMsDuration(str) {
+    return str + ':00'
+}
 
