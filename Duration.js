3a716be9a2e311736969cbba28608086f80cf99e
Peter Sharp <peter.victor.sharp@gmail.com>
2018-12-11 22:44:55 +1300

    converted time object to milliseconds untested

--- Duration.js
+++ Duration.js
@@ -3,7 +3,22 @@ const Duration = {}
 
 
 Duration.fromObject = function(obj) {
-    const state = Object.assign({hours: 0, minutes: 0, seconds: 0}, obj);
+    let duration = toMilliseconds(Object.assign({hours: 0, minutes: 0, seconds: 0}, obj));
+
+    function toMilliseconds(obj) {
+        const MILLISECONDS_PER_SECOND = 1000
+        const SECONDS_PER_MINUTE = 60
+        const MINUTES_PER_HOUR = 60
+
+        let milliseconds = 0
+
+        milliseconds += obj.seconds * MILLISECONDS_PER_SECOND
+        milliseconds += obj.minutes * SECONDS_PER_MINUTE * MILLISECONDS_PER_SECOND
+        milliseconds += obj.hours * MINUTES_PER_HOUR * SECONDS_PER_MINUTE * MILLISECONDS_PER_SECOND
+
+        return milliseconds
+    }
+
     return {
         get minutes() {return state.minutes },
         as(unit) { return 0},
